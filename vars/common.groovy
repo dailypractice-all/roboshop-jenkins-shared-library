649076e4c8e63299addb257c19473c33bd50e3b3
gaurav1996b <1996gauravb@gmail.com>
2022-07-07 10:41:55 +0530

    updating jenkins file

--- vars/common.groovy
+++ vars/common.groovy
@@ -1,6 +1,6 @@
 def pipelineInit() {
   stage('Initiate Repo') {
     sh 'rm -rf *'
-    git branch: 'main', url: 'https://github.com/dailypractice-all/cart.git'
+    git branch: 'main', url: "https://github.com/dailypractice-all/${COMPONENT}.git"
   }
 }
