99ff115d98a079778ddf45ceb8e9cb3837a4bc0f
gaurav1996b <1996gauravb@gmail.com>
2022-07-07 15:51:29 +0530

    updating jenkins file

--- vars/common.groovy
+++ vars/common.groovy
@@ -16,7 +16,7 @@ def publishArtifacts() {
   stage('Push Artifacts to Nexus') {
     withCredentials([usernamePassword(credentialsId: 'NEXUS', passwordVariable: 'pass', usernameVariable: 'user')]) {
       sh """
-        curl -v -u ${user}:${pass} --upload-file ${COMPONENT}-${TAG_NAME}.zip http://172.31.7.89:8081/repository/${COMPONENT}/${COMPONENT}-${TAG_NAME}.zip
+        curl -v -u ${user}:${pass} --upload-file ${COMPONENT}-${TAG_NAME}.zip http://172.31.11.249:8081/repository/${COMPONENT}/${COMPONENT}-${TAG_NAME}.zip
       """
     }
   }
