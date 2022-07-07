c6387aae4864949482a453187b0811cd7ea4251d
gaurav1996b <1996gauravb@gmail.com>
2022-07-07 17:47:56 +0530

    updating jenkins file

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -7,9 +7,7 @@ def call() {
         mvn clean package
       '''
     }
-
-    if( env.BRANCH_NAME == env.TAG_NAME )
-    {
+    if (env.BRANCH_NAME == env.TAG_NAME) {
       common.publishArtifacts()
+    }
   }
-}
