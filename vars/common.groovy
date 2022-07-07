19cdc0dc3c2eaa46932f5af35ca765ac39712059
gaurav1996b <1996gauravb@gmail.com>
2022-07-07 16:09:42 +0530

    updating jenkins file

--- vars/common.groovy
+++ vars/common.groovy
@@ -9,15 +9,16 @@ def publishArtifacts() {
   stage("Prepare Artifacts") {
     if (env.APP_TYPE == "nodejs") {
       sh """
-        zip -r ${COMPONENT}.zip node_modules server.js
-      """
-    }
-  }
-  stage('Push Artifacts to Nexus') {
-    withCredentials([usernamePassword(credentialsId: 'NEXUS', passwordVariable: 'pass', usernameVariable: 'user')]) {
-      sh """
-        curl -v -u ${user}:${pass} --upload-file ${COMPONENT}-${TAG_NAME}.zip http://172.31.11.249:8081/repository/${COMPONENT}/${COMPONENT}-${TAG_NAME}.zip
+        zip -r ${COMPONENT}-${TAG_NAME}.zip node_modules server.js
       """
     }
   }
 }
+//  stage('Push Artifacts to Nexus') {
+//    withCredentials([usernamePassword(credentialsId: 'NEXUS', passwordVariable: 'pass', usernameVariable: 'user')]) {
+//      sh """
+//        curl -v -u ${user}:${pass} --upload-file ${COMPONENT}-${TAG_NAME}.zip http://172.31.11.249:8081/repository/${COMPONENT}/${COMPONENT}-${TAG_NAME}.zip
+//      """
+//    }
+//  }
+//}
--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,18 +1,20 @@
 def call() {
   node() {
+
     common.pipelineInit()
 
     stage('Download Dependencies') {
       sh '''
         ls -ltr
-        npm install
+        npm install 
       '''
     }
 
-    //If both are equal it is definitely a tag
-    if ( env.BRANCH_NAME == env.TAG_NAME )
+    // If both are equal then it is definitely a tag
+    if( env.BRANCH_NAME == env.TAG_NAME )
     {
       common.publishArtifacts()
     }
+
   }
 }
