878e63a8e0d4180dc07e37610a35d4bcda130457
gaurav1996b <1996gauravb@gmail.com>
2022-07-28 18:29:26 +0530

    updating terraform file with roboshop project

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -1,17 +1,22 @@
 def call() {
   node {
+    properties([
+      parameters([
+        choice(choices: ['dev', 'prod'], description: "Choose Environment", name: "ENV"),
+      ])
+    ])
     ansiColor('xterm') {
       stage('Terraform Init') {
-        sh 'terraform init'
+        sh 'terraform init -backend-config=env/${ENV}-backend.tfvars'
       }
       stage('Terraform Validate') {
         sh 'terraform validate'
       }
       stage('terraform Plan') {
-        sh 'terraform plan'
+        sh 'terraform plan -var-file=env/${ENV}.tfvars'
       }
       stage('Terraform Apply') {
-        sh 'terraform apply -auto-approve'
+        sh 'terraform apply -auto-approve -var-file=env/${ENV}.tfvars'
       }
     }
   }
