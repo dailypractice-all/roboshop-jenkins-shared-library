7aba72ab78f7f84e9b7f1dc06804dbe60b347573
gaurav1996b <1996gauravb@gmail.com>
2022-07-29 13:10:12 +0530

    updating terraform file with roboshop project

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -8,7 +8,7 @@ def call() {
     ansiColor('xterm') {
 
       stage('Code Checkout') {
-        sh 'rm -rf *'
+        sh 'find . | sed -e "1d | xargs rm -rf'
         git branch: 'main', url: 'https://github.com/dailypractice-all/roboshop-terraform-mutable-approach.git'
       }
       stage('Terraform Init') {
